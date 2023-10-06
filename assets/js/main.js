e578b2e3de989103ffba49e5e9c8bad5ac3e0194
yasmeenbsharat <y.bsharat@student.aaup.edu>
2023-10-06 21:16:01 +0300

    Add debounce function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
-
+let htmlCode = document.getElementById('html-code');
+let cssCode = document.getElementById('css-code');
+let jsCode = document.getElementById('js-code');
 let output = document.getElementById('output');
-function runCode() {
-
-
-    let htmlCode = document.getElementById('html-code').value;
-    let cssCode = document.getElementById('css-code').value;
-    let jsCode = document.getElementById('js-code').value;
-    output.contentDocument.body.innerHTML = `${htmlCode} <style> ${cssCode} </style>`;
-    output.contentWindow.eval(jsCode);
-    console.log(htmlCode);
-
+function debounce(func, delay) {
+    let timeoutId;
+    return function () {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(func, delay);
+    };
 }
+
+
+function runCode() {
+    output.contentDocument.body.innerHTML = `${htmlCode.value} <style> ${cssCode.value} </style>`;
+    output.contentWindow.eval(jsCode.value);
+}
+
+
+const debouncedRunCode = debounce(runCode, 500);
+htmlCode.addEventListener('input', debouncedRunCode);
+cssCode.addEventListener('input', debouncedRunCode);
+jsCode.addEventListener('input', debouncedRunCode);
